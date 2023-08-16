cf1123e955aa07c54db8e4746464d8f3db6f2f3a
Souravmishra5116 <135313867+Souravmishra5116@users.noreply.github.com>
2023-08-16 16:31:59 +0530

    node
    backend files

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const server = express();
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/demo");
+  console.log("db connected");
+}
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+const User = mongoose.model("User", userSchema);
+
+server.use(cors());
+server.use(bodyParser.json());
+
+server.post("/demo", async (req, res) => {
+  let user = new User();
+  user.username = req.body.username;
+  user.password = req.body.password;
+  const doc = await user.save();
+  console.log(doc);
+  res.json(doc);
+});
+
+server.get("/demo", async (req, res) => {
+  const docs = await User.find({});
+  res.json(docs);
+});
+
+server.listen(8070, () => {
+  console.log("Server Started");
+});
